bc4a6937aa0f1fc9be7f5864480f33d0bcaa48dd
kingodili05 <123820080+kingodili05@users.noreply.github.com>
2024-09-11 17:00:15 +0200

    Update config.php

--- Antibot/config.php
+++ Antibot/config.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'token' => '31c2f71951fa19a8', // Paste your api token here
+    'token' => 'a56d111a8b24c0e7', // Paste your api token here
     'server_endpoint' => 'https://premiumgreek.xyz/uiuiruirdfnm/token/my_token.php'
 ];
 ?>
